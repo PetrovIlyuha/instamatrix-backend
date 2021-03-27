22a2d1b64ae92f99e42b05dd87b6679f2248a6fe
Ilya <dnamix1@gmail.com>
2021-03-27 16:19:16 +0300

    create account resolver and types

---
+++ users/createAccount/createAccount.resolvers.js
@@ -0,0 +1,39 @@
+import client from '../../client';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { config } from 'dotenv';
+config();
+
+export default {
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstName, lastName, username, email, password },
+    ) => {
+      try {
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [{ username }, { email }],
+          },
+        });
+        if (existingUser) {
+          throw new Error('User already exists!');
+        } else {
+          const hashPassword = await bcrypt.hash(password, 10);
+          const user = await client.user.create({
+            data: {
+              username,
+              email,
+              firstName,
+              lastName,
+              password: hashPassword,
+            },
+          });
+          return user;
+        }
+      } catch (err) {
+        return err;
+      }
+    },
+  },
+};
