b369bf74989fda86242d5ec37d9af82b743040d0
Ilya <dnamix1@gmail.com>
2021-04-05 15:13:40 +0300

    added packages for future typescript migration and utility functions for user behaviour handling

---
+++ users/users.utils.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+import client from '../client';
+
+export async function getUser(token) {
+  try {
+    if (!token) {
+      return null;
+    } else {
+      const { id } = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await client.user.findUnique({ where: { id } });
+      if (user) {
+        return user;
+      } else {
+        return null;
+      }
+    }
+  } catch (err) {
+    return null;
+  }
+}
+
+export const authGuard = resolver => (root, args, context, info) => {
+  if (!context.loggedInUser) {
+    return {
+      ok: false,
+      error: 'You are not logged in!',
+    };
+  }
+  return resolver(root, args, context, info);
+};
