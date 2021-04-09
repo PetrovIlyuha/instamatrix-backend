679e184a959a70d51991aee42ac38874edb78381
Ilya <dnamix1@gmail.com>
2021-04-09 14:05:18 +0300

    aws integration and server implemented to be able to use apollo's pubsub system

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import { config } from 'dotenv';
+import http from 'http';
 import express from 'express';
 import logger from 'morgan';
 config();
+
+import pubsub from './pubsub.js';
 
 import { ApolloServer } from 'apollo-server-express';
 import { resolvers, typeDefs } from './schema.js';
@@ -11,10 +14,25 @@ async function startServer() {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: async ({ req }) => {
-      const token = req.headers.auth || '';
-      const user = await getUser(token);
-      return { loggedInUser: user, authGuard };
+    context: async ctx => {
+      if (ctx.req) {
+        const token = ctx.req.headers.auth || '';
+        const user = await getUser(token);
+        return { loggedInUser: user, authGuard };
+      } else {
+        return { loggedInUser: ctx.connection.context.loggedInUser };
+      }
+    },
+    subscriptions: {
+      onConnect: async ({ auth }) => {
+        if (!auth) {
+          throw new Error(
+            'You are not allowed in the chats! Login, please, dear hacker!',
+          );
+        }
+        const user = await getUser(auth);
+        return { loggedInUser: user };
+      },
     },
   });
   await server.start();
@@ -24,8 +42,11 @@ async function startServer() {
   app.use(logger('tiny'));
   server.applyMiddleware({ app });
 
+  const httpServer = http.createServer(app);
+  server.installSubscriptionHandlers(httpServer);
+
   await new Promise(resolve =>
-    app.listen({ port: process.env.SERVER_PORT }, resolve),
+    httpServer.listen({ port: process.env.SERVER_PORT }, resolve),
   );
   console.log(
     `🚀 Server ready at http://localhost:${process.env.SERVER_PORT}${server.graphqlPath}`,
