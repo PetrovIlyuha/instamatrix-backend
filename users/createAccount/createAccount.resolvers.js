bcd47c5f73c837347435351762ae1fbc362c1df8
Ilya <dnamix1@gmail.com>
2021-04-11 12:10:15 +0300

    add basic validations for create account resolver

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -19,6 +19,24 @@ export default {
         if (existingUser) {
           throw new Error('User already exists!');
         } else {
+          if (firstName < 2) {
+            return {
+              ok: false,
+              error: 'First name is too short. 2 characters min',
+            };
+          }
+          if (username.length < 3) {
+            return {
+              ok: false,
+              error: 'Username too short. 3 characters min',
+            };
+          }
+          if (password.length < 6) {
+            return {
+              ok: false,
+              error: 'Password should be longer than 6 characters',
+            };
+          }
           const hashPassword = await bcrypt.hash(password, 10);
           const user = await client.user.create({
             data: {
